385b9ea84bf324d4b3db082ff9bab55d887ced56
Ya_Em_Salo <narancha.ar@gmail.com>
2021-12-13 16:41:59 +0200

    python

---
+++ First_Lesson/work/program.py
@@ -0,0 +1,23 @@
+print("Hello World????")
+a = 10
+b = 2
+print("Это А :", a)
+input()
+
+x = a + b
+v = b - a
+
+h = input("Ваше имя? ")
+print("Здравствуйте,",h, "приятно познакомиться)))")
+j = input("А лет сколько? ")
+print(h,",","тебе ",j," лет ")
+
+
+num = int(input("Угадай число "))
+res = 2
+if num == res :
+    print("Угадал!!!")
+elif num > res:
+    print("Твое число больше")
+else :
+    print("Твое число меньше")
