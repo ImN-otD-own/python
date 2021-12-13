e09eb30bbb45510b02df93aa723375db7d298b17
Ya_Em_Salo <narancha.ar@gmail.com>
2021-12-13 18:28:00 +0200

    python

---
+++ lesson2/classwork/file.py
@@ -0,0 +1,80 @@
+import random
+
+""" print() """
+
+print(1)
+print(1, 2)
+
+print("Один", 1, "Два", 2)
+
+a = 5
+b = 3
+c = 10
+
+print(a, b, c)
+
+""" print() """
+
+##
+
+""" input() """
+
+name = input("PLease, input your name: ")
+print(f"Hello there, {name}")
+
+""" input() """
+
+##
+
+""" math """
+
+num = 10
+
+num += 3
+num -= 3
+num *= 2
+num**2
+
+print(num)
+
+""" math """
+
+##
+
+""" str """
+
+t = str(input("Введите число"))
+i = str(input("Введите число"))
+print(t + i)
+
+""" str """
+
+##
+
+""" bool """
+
+g = int(input())
+print(bool(g))
+
+""" bool """
+
+##
+
+""" list """
+
+j = 1
+o = 9
+u = 8
+print(list(j , o , u))
+
+""" list """
+
+##
+
+""" random """
+
+rnd = [1, 5 ,7 ,2 ,5, 3, 8]
+rand = random.choice(rnd)
+print(rand)
+
+""" random """
